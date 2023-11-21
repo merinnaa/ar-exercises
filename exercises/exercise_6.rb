c9669afad0282957e452fe781f17c23ed001699c
luwam <lwamyohannes2018@gmail.com>
2023-11-20 17:28:41 -0700

    valodation added in ex7

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,9 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "devid", last_name: "Vira", hourly_rate: 40)
+@store1.employees.create(first_name: "john", last_name: "william", hourly_rate: 50)
+@store2.employees.create(first_name: "will", last_name: "liam", hourly_rate: 50)
+@store2.employees.create(first_name: "smith", last_name: "bill", hourly_rate: 50)
+@store2.employees.create(first_name: "johnson", last_name: "williard", hourly_rate: 50)
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,32 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+validates :first_name, presence: true
+validates :last_name, presence: true
+validates :hourly_rate, numericality: { only_integer: true, greater_than_or_equal_to: 40, less_than_or_equal_to: 200}
+validates :store, presence: true
+validates :name , presence: true, length: {minimum: 3}
+validates :annual_revenue, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+
+validates :must_cary_apreal
+
+def must_cary_apreal
+  unless mens_apparel || womens_apparel
+    errors.add(:base, 'must include atleast one mens or womens apreals')
+  end
+end
+belongs_to :store
+
+puts "inter your name"
+store_name = gets.chomp
+new_store = Store.new(name: store_name)
+
+if new_store.save
+  puts "success"
+else 
+  puts "error:"
+  new_store.errors.full_message.each do |message|
+    puts message
+  end
+end
+
